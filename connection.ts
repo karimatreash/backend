30bde9f98d30326e938ed102dba5919ace4968ed
karim <atrashkareem@gmail.com>
2024-05-13 21:51:22 +0300

    new

--- connection.ts
+++ connection.ts
@@ -2,14 +2,11 @@ import mysql from 'mysql';
 import calculateAndSetRatio from './func/sumrating';
 import calculateAndSetcout from './func/coutn_request';
 const conn = mysql.createConnection({
-   // host: 'localhost',
-   // user: 'root',
-   // password: '',
-   // database: 'home_services'
-   host: 'sql6.freesqldatabase.com',
-   user: 'sql6704270',
-   password: 'vpP2TYJwvz',
-   database: 'sql6704270'
+   host: 'localhost',
+   user: 'root',
+   password: '',
+   database: 'home_services'
+ 
 
 });
 conn.connect((err) => {
